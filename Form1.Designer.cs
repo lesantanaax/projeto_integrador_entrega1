5c888c4c10cc716a1ce8ee3ba727ef3e9ecae817
Bruno Oliveira <brunooliveiraalves78@gmail.com>
2026-05-03 19:41:39 -0300

    ajuste

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -407,7 +407,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Aquamarine;
-            this.ClientSize = new System.Drawing.Size(900, 535);
+            this.ClientSize = new System.Drawing.Size(1431, 715);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAdicionarJogador);
             this.Controls.Add(this.btnIniciar);
