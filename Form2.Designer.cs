b23ddffd1bc9156d59c7712b1a2dd58081af304f
Bruno Oliveira <brunooliveiraalves78@gmail.com>
2026-06-06 21:59:16 -0300

    Ajuste campo entrar em partida

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -37,11 +37,13 @@
             this.nomePartida = new System.Windows.Forms.Label();
             this.senha = new System.Windows.Forms.Label();
             this.lblLobby = new System.Windows.Forms.Label();
+            this.idPartida = new System.Windows.Forms.Label();
+            this.txtiddapartida = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txtnome
             // 
-            this.txtnome.Location = new System.Drawing.Point(313, 208);
+            this.txtnome.Location = new System.Drawing.Point(313, 140);
             this.txtnome.Margin = new System.Windows.Forms.Padding(2);
             this.txtnome.Multiline = true;
             this.txtnome.Name = "txtnome";
@@ -61,9 +63,9 @@
             // 
             this.Btnentrar.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.Btnentrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btnentrar.Location = new System.Drawing.Point(632, 315);
+            this.Btnentrar.Location = new System.Drawing.Point(614, 308);
             this.Btnentrar.Name = "Btnentrar";
-            this.Btnentrar.Size = new System.Drawing.Size(81, 33);
+            this.Btnentrar.Size = new System.Drawing.Size(122, 54);
             this.Btnentrar.TabIndex = 21;
             this.Btnentrar.Text = "ENTRAR";
             this.Btnentrar.UseVisualStyleBackColor = false;
@@ -95,7 +97,7 @@
             this.lblNomeJogador.AutoSize = true;
             this.lblNomeJogador.BackColor = System.Drawing.SystemColors.Info;
             this.lblNomeJogador.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNomeJogador.Location = new System.Drawing.Point(45, 211);
+            this.lblNomeJogador.Location = new System.Drawing.Point(45, 147);
             this.lblNomeJogador.Name = "lblNomeJogador";
             this.lblNomeJogador.Size = new System.Drawing.Size(233, 25);
             this.lblNomeJogador.TabIndex = 25;
@@ -129,11 +131,32 @@
             this.lblLobby.AutoSize = true;
             this.lblLobby.BackColor = System.Drawing.SystemColors.Info;
             this.lblLobby.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLobby.Location = new System.Drawing.Point(183, 75);
+            this.lblLobby.Location = new System.Drawing.Point(192, 42);
             this.lblLobby.Name = "lblLobby";
             this.lblLobby.Size = new System.Drawing.Size(418, 37);
             this.lblLobby.TabIndex = 28;
             this.lblLobby.Text = "LOBBY DRAFTOSSAURUS";
+            // 
+            // idPartida
+            // 
+            this.idPartida.AutoSize = true;
+            this.idPartida.BackColor = System.Drawing.SystemColors.Info;
+            this.idPartida.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idPartida.Location = new System.Drawing.Point(103, 208);
+            this.idPartida.Name = "idPartida";
+            this.idPartida.Size = new System.Drawing.Size(175, 25);
+            this.idPartida.TabIndex = 29;
+            this.idPartida.Text = "ID DA PARTIDA :";
+            this.idPartida.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtiddapartida
+            // 
+            this.txtiddapartida.Location = new System.Drawing.Point(313, 201);
+            this.txtiddapartida.Margin = new System.Windows.Forms.Padding(2);
+            this.txtiddapartida.Multiline = true;
+            this.txtiddapartida.Name = "txtiddapartida";
+            this.txtiddapartida.Size = new System.Drawing.Size(182, 32);
+            this.txtiddapartida.TabIndex = 30;
             // 
             // Form2
             // 
@@ -143,6 +166,8 @@
             this.BackgroundImage = global::projeto_integrador_entrega1.Properties.Resources.draftosaurus_spel_koning_van_het_oerwoud;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(805, 438);
+            this.Controls.Add(this.txtiddapartida);
+            this.Controls.Add(this.idPartida);
             this.Controls.Add(this.lblLobby);
             this.Controls.Add(this.senha);
             this.Controls.Add(this.nomePartida);
@@ -169,5 +194,7 @@
         private System.Windows.Forms.Label nomePartida;
         private System.Windows.Forms.Label senha;
         private System.Windows.Forms.Label lblLobby;
+        private System.Windows.Forms.Label idPartida;
+        private System.Windows.Forms.TextBox txtiddapartida;
     }
 }
